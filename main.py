24c379c8c2cd84cb0b694ca2af4b81c4f015ff70
addirossi <addirossi@gmail.com>
2022-08-02 08:39:30 +0600

    Add main.py

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+import os
+
+from dotenv import load_dotenv
+
 from bot import VkBot
 
 
+load_dotenv()
+
+
+user_token = os.getenv('ACCESS_TOKEN')
+group_token = os.getenv('GROUP_TOKEN')
+
+
 bot = VkBot(user_token, group_token)
+bot.start()
